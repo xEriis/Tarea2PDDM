e788903d8e3d82b6cbebd9eb64099b0bbd538295
xEriis <Eris@ciencias.unam.mx>
2025-03-03 07:33:48 -0600

    Avance de app

--- app/src/main/java/com/erynlexa/tarea2/MainActivity.kt
+++ app/src/main/java/com/erynlexa/tarea2/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.erynlexa.tarea2
 
 import android.os.Bundle
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,25 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val botonVistaUno : Button = findViewById(R.id.botonV1)
+        botonVistaUno.setOnClickListener {
+            val aperturaVistaUno = Intent(this, PantallaUno::class.java)
+            startActivity(aperturaVistaUno)
+        }
+
+        val botonVistaDos : Button = findViewById(R.id.botonV2)
+        botonVistaDos.setOnClickListener {
+            val aperturaVistaDos = Intent(this, PantallaDos::class.java)
+            startActivity(aperturaVistaDos)
+        }
     }
+
 }
